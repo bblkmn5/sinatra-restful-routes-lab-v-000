0d1be5135549c15d75b22e0b943759d3c3076aee
Ben Blackman <bblkmn5@gmail.com>
2017-12-07 19:56:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+  get '/' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
 end
